bdc093a0c94ea54850bbd8588cf773e25059298b
BukLauBokChoi <xinglu.py@gmail.com>
2018-01-31 11:58:29 +0100

    done

--- Rock_Paper_Scissors_1_v_1.py
+++ Rock_Paper_Scissors_1_v_1.py
@@ -2,6 +2,27 @@
 this is a rock paper scissors game where you can play against your friend!
 """
 
-player1 = str(input("Player 1 chose :"))
-player2 = str(input("Player 2 chose :"))
 
+def Play_game():
+    # players
+    player1_name = str(input("What is your name?"))
+    player2_name = str(input("What is your name?"))
+
+    player1 = str(input("Player 1 chose :"))
+    player2 = str(input("Player 2 chose :"))
+
+
+
+    if player1 == player2:
+        print("it's a tie")
+    elif player1 == "paper" and player2 == "rock" or player1 == "rock" and player2 == "scissors" or player1 == "scissors" and player2 == "paper":
+        print(f"{player1_name} won the game!")
+    else:
+        print(f"{player2_name} won the game!")
+
+
+# how many times do you want to play
+rounds = int(input("How many times do you want to play?"))
+
+for i in range(rounds):
+    Play_game()
